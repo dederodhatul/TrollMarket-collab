6b978280d448e2061c7b6b8ec073271f7435a024
dederodhatul <dederodhatulfarida@gmail.com>
2022-12-14 14:08:37 +0700

    adding, update html and adding controller

---
+++ src/main/java/com/trollmarket/controller/BuyerController.java
@@ -0,0 +1,2 @@
+package com.trollmarket.controller;public class BuyerController {
+}
---
+++ src/main/resources/static/js/shop.js
@@ -0,0 +1,22 @@
+//var addProduct = document.querySelector('.add-product');
+//var formAddProduct = document.querySelector('.form-addProduct');
+//
+//addProduct.addEventListener('click', function(){
+//    formAddProduct.style.visibility = "visible";
+//})
+//
+//var detailProduct = document.querySelector(".detail-product");
+//var formDetailProduct = document.querySelector(".popup-detail-product");
+//
+//detailProduct.addEventListener('click', function(){
+//    formDetailProduct.style.visibility = "visible";
+//})
+
+var infoMerchand = document.querySelector(".info")
+//var formInfoMerchand = document.querySelector(".form-info");
+
+//infoMerchand.addEventListener('click', function(){
+//    formInfoMerchand.style.visibility = "visible";
+//})
+
+
